34f3197a59708185b7cb4e24ebe458f3cd7ecca7
E Tidalgo <etidalgo@gmail.com>
2016-01-17 22:48:34 +1300

    Collapse concat usage

--- FlattenUtil.js
+++ FlattenUtil.js
@@ -2,37 +2,15 @@
 // var sample = [ [1,2], [[3,[4]], 5], [6,7,8], 9, 10];
 // var result = arr.reduce( _toFlat, [] );
 
-
 var _toFlat = function( previous, current ) { 
-	if (!Array.isArray(current)) {
-		previous.push(current);
-		return previous;
+	var nextElement;
+	if (Array.isArray(current)) {
+		nextElement = current.reduce( _toFlat, [] );
+	} else {
+		nextElement = current;
 	}
-	
-	var newArr = current.reduce( _toFlat, [] );
-	return previous.concat(newArr);
+	return previous.concat(nextElement);
 };
 
 // Array.prototype.reduce() - JavaScript | MDN <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce>
-
-// Destructive flattening
-function _flattenArray( arr ) { 
-	if (!Array.isArray(arr)) {
-		return [arr];
-	}
-
-	var result = [];
-	var elem1 = arr[0];
-	if (!Array.isArray(elem1)) {
-		result.push(elem1);
-		} else {
-		result = result.concat( _flattenArray(elem1) );
-		}
-
-    if (arr.length > 1){
-    	arr.splice(0, 1);
-    	result = result.concat( _flattenArray( arr ) );
-    }
-    
-	return result;
-}
+//Array.prototype.concat() - JavaScript | MDN <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat>
